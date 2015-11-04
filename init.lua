343d74857587a31c8dfd4b48dc57e4f6a36bbfbd
Schlomo Schapiro <schlomo+git@schapiro.org>
2015-11-04 19:24:34 +0100

    Add some screen layout magic

--- init.lua
+++ init.lua
@@ -2,16 +2,54 @@ require "tabletools"
 
 hs.window.animationDuration=0
 
+-- my magic shortcut base
 local ctrlaltcmd = {"⌃", "⌥", "⌘"}
+-- Define monitor names for layout purposes
+local display_laptop = "Color LCD"
+local display_desk_right = "DELL 2408WFP - forced RGB mode (EDID override)"
+local display_desk_left = "DELL U2410 - forced RGB mode (EDID override)"
 
+-- Define window layouts
+--   Format reminder:
+--     {"App name", "Window name", "Display Name", "unitrect", "framerect", "fullframerect"},
+local internal_display = {
+    {"Mail",              nil,          display_laptop, hs.layout.maximized, nil, nil},
+    {"Microsoft Outlook", nil,          display_laptop, hs.layout.maximized, nil, nil},
+    {"Calendar",          nil,          display_laptop, hs.layout.maximized, nil, nil},
+    {"Evernote",          nil,          display_laptop, hs.layout.maximized, nil, nil},
+    {"iTerm",            nil,           display_laptop, hs.layout.maximized, nil, nil},
+}
+
+local desk_display = {
+    {"Mail",              nil,          display_desk_left,  hs.layout.right50, nil, nil},
+    {"Microsoft Outlook", nil,          display_desk_left, hs.layout.maximized, nil, nil},
+    {"Calendar",          nil,          display_desk_left, hs.layout.left50, nil, nil},
+    {"Jabber",            nil,          display_desk_left,  hs.layout.maximized, nil, nil},
+    {"Evernote",          nil,          display_desk_right, hs.layout.maximized,   nil, nil},
+    {"iTerm",             nil,          display_desk_right, hs.layout.maximized,   nil, nil},
+}
 
 function notify(text)
     hs.notify.new({title="Hammerspoon", informativeText=text}):send()
 end
 
+local lastNumberOfScreens = #hs.screen.allScreens()
 function screenWatcher()
-    notify("Screens changed\n")
     print(table.show(hs.screen.allScreens(), "allScreens"))
+    newNumberOfScreens = #hs.screen.allScreens()
+
+    -- FIXME: This is awful if we swap primary screen to the external display. all the windows swap around, pointlessly.
+    if lastNumberOfScreens ~= newNumberOfScreens then
+        if newNumberOfScreens == 1 then
+            print("Internal Display")
+            hs.layout.apply(internal_display)
+        elseif newNumberOfScreens == 2 then
+            print("Desk Display")
+            hs.layout.apply(desk_display)
+        end
+    end
+
+    lastNumberOfScreens = newNumberOfScreens
 end
 hs.screen.watcher.new(screenWatcher):start()
 
